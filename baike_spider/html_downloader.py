4ea2273f2b910c8f28649f30ed68bca9c1eda8eb
zth <1814942875@qq.com>
2018-12-07 10:26:58 +0800

    百度百科

---
+++ baike_spider/html_downloader.py
@@ -0,0 +1,14 @@
+import urllib
+
+class HtmlDownloader(object):
+
+    def download(self,url):
+        if url is None:
+            return None
+
+        response=urllib.request.urlopen(url)
+
+        if response.getCode()!=200:
+            return None
+
+        return response.read()
